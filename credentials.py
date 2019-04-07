07e741292ec1347668ab72d3252aecfd8c371a46
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-07 23:18:48 +0300

    corrected a bug

--- credentials.py
+++ credentials.py
@@ -13,14 +13,15 @@ class Credential:
     user_credentials_list = []
 
     @classmethod
-    def authenticate_user(cls,first_name,last_name,password):
+    def authenticate_user(cls,first_name,password):
         '''
         A method that checks if the name and username entered match those in the entries in user_list
         '''
+        current_user = ""
 
         for user in User.user_list:
-            if (user.first_name == first_name and user.last_name == last_name and user.password == password):
-                current_user = user.user_name
+            if (user.first_name == first_name and user.password == password):
+                current_user = user.first_name
 
             return current_user
 
@@ -41,12 +42,12 @@ class Credential:
 
         Credential.credentials_list.append(self)
 
-    def generate_password(self):
+    def generate_password(stringlength=10):
         '''
         method that generates a random password
         '''
         letters = string.ascii_letters + string.digits
-        gen_pass = ''.join(random.choice(letters) for i in range (10))
+        gen_pass = ''.join(random.choice(letters) for i in range (stringlength))
         return gen_pass
 
     @classmethod
