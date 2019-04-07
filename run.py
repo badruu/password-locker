e7c198cf83315b0fdc7cb5c77f7704dc8f0a59cc
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-07 23:28:00 +0300

    add elif commands for the first while loo

--- run.py
+++ run.py
@@ -40,14 +40,14 @@ def create_credential(user_name,media_site,account_name,password):
     function creating a new credential
     '''
 
-    new_credential = Credential(user_name,site_name,account_name,password)
+    new_credential = Credential(user_name,media_site,account_name,password)
     return new_credential
 
 def save_credential(credential):
     '''
     function that saves a newly created credential
     '''
-    Credential.save_credential(credential)
+    Credential.save_credentials(credential)
 
 def display_credentials(user_name):
     '''
@@ -71,7 +71,7 @@ def main():
 
     while True:
         # print("\n")
-        print("-"*20)
+        print("-"*50)
         print("""What would you like to do?
          \n
           Use the codes below
@@ -96,9 +96,118 @@ def main():
             save_user(create_user(first_name,last_name,password))
 
             print("")
+            print("-"*50)
             print(f"You have created a new account for: {first_name} {last_name} \n This is your password: {password} ")
-            continue
-        
+            
+
+        elif short_code == "lg":
+            print("-"*50)
+            print("\n")
+            print("To login, provide you account information: ")
+
+            user_name = input("Enter your first name -- ").strip()
+            password = input("Enter your password -- ")
+
+            user_exists = authentication(user_name,password)
+
+            if user_exists == user_name:
+                print(" ")
+                print(f"Welcome {user_name}. We are glad to have you again.")
+                print(" ")
+                while True:
+                    print("-"*50)
+                    # print("Navigation: \n cc")
+                    print("""Choose an Option from the list below: \n
+                        1) cc - create a credential
+                        2) dc - display credentials
+                        3) cp - copy password of a credential
+                        4) ex - exit
+                    """)
+                    short_code = input("Option chosen: ").lower().strip()
+                    print("-"*50)
+
+                    if short_code == "ex":
+                        print(" ")
+                        print(f"Goodluck {user_name}")
+                        break
+
+                    elif short_code == "cc":
+                        print(" ")
+                        print("-"*50)
+                        print("Enter the details of the account you would like to create:")
+                        media_site = input("Enter the site\'s name -- ").strip()
+                        account_name = input("Enter your account name -- ").strip()
+                        while True:
+                            print(" ")
+                            print("-"*50)
+                            print("""How would you like you password to be genrate:
+                                1) exp - for enter an existing password
+                                2) gn - for generate a password automatically
+                                3) ex - exit
+                            """)
+                            password_choice = input("Enter an option: ").lower().strip()
+                            print("-"*50)
+                            if password_choice == "exp":
+                                print(" ")
+                                password = input("Enter your password: ").strip()
+                                break
+
+                            elif password_choice == "gn":
+                                password = generate_password()
+                                break
+
+                            elif password_choice == "ex":
+                                break
+
+                            else:
+                                print("Damn! Why don\'nt you follow simple instruction(!)")
+
+                        save_credential(create_credential(user_name,media_site,account_name,password))
+                        print(" ")
+                        print(f"""You have created the following credential:
+                        Media site: {media_site}
+                        Account name: {account_name}
+                        Password: {password}""")
+                        print(" ")
+
+                    elif short_code == "dc":
+                        print(" ")
+                        if display_credentials(user_name):
+                            print("Below is a list of all your credentials")
+                            print(" ")
+                            for credential in display_credentials(user_name):
+                                print(f"""
+                                media site: {credential.media_site}
+                                account name: {credential.account_name}
+                                password: {credential.password}
+                                """)
+                            print(" ")
+
+                        else: 
+                            print(" ")
+                            print("You haven\'t yet saved any credentials")
+                            print(" ")
+
+                    elif short_code == "cp":
+                        print(" ")
+                        chosen_site = input("Enter the site name for the credential password tou want to copy: ")
+                        copy_credential(chosen_site)
+                        print(" ")
+
+                    else: 
+                        print("""You don\'t seem to follow instructions, do you?
+                        Enter choices from the given options only!!
+                        """)
+                        break
+
+            else:
+                print(" ")
+                print("Try again or create an account!")
+
+        else: 
+            print(" ")
+            print("You haven\'nt entered options provided")
+
 
 
 if __name__ == "__main__":
