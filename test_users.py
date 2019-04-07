3b4b31fe45f6483ed711411e4e6e372309f9746d
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-07 18:27:26 +0300

    change name from users_test & modifications for user_name

---
+++ test_users.py
@@ -0,0 +1,35 @@
+import unittest
+from users import User
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behavior.
+
+    Args:
+        unittest.Testcase: helps in creating test cases
+    '''
+
+    def setUp(self):
+        '''
+        set up method to run before each test cases.
+        '''
+        self.new_user = User("badar","noor","badar124")
+
+    def test_init(self):
+        '''
+        test case to check if the object is initialized properly.
+        '''
+        self.assertEqual(self.new_user.first_name,"badar")
+        self.assertEqual(self.new_user.last_name,"noor")
+        self.assertEqual(self.new_user.password,"badar124")
+
+    def test_save_user(self):
+        '''
+            test case to check if the user object is saved into the user list
+        '''
+
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+if __name__ =='__main__':
+    unittest.main()
